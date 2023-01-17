fbf7824829540048efa441c70dc5a111c68010a7
unknown <ejoannamonserrat@gmail.com>
2023-01-16 22:41:23 -0600

    reto1 final

--- reto1.js
+++ reto1.js
@@ -4,5 +4,18 @@ function wrapping(gifts) {
     const wrap = '*'.repeat(gifts[i].length + 2);
   giftsWrapped.push(`${ wrap }\n*${ gifts[i] }*\n${ wrap }`);
   }
+  for(gift in gifts){
+    const wrap = '*'.repeat(gift.length + 2);
+    giftsWrapped.push(`${ wrap }\n*${ gifts[i] }*\n${ wrap }`);
+  }
+  gifts.forEach( gift => {
+    const wrap = '*'.repeat(gift.length + 2);
+    giftsWrapped.push(`${ wrap }\n*${ gifts[i] }*\n${ wrap }`);
+  });
   return giftsWrapped;
 }
+
+const gifts = ['cat', 'game', 'socks'];
+const wrapped = wrapping(gifts);
+
+console.log(wrapped[0]);
